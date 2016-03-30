c8377723a708853cf003dae0ad42709cf054f9b0
Abinav Seelan <abinav.n.seelan@gmail.com>
2016-03-31 01:04:13 +0530

    Second Push

---
+++ random_forest.py
@@ -0,0 +1,56 @@
+import sys
+import numpy as np
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
+
+dataset = np.loadtxt("normalized.csv", delimiter=",")
+
+train_X = dataset[1:10000,0:10]
+train_Y = dataset[1:10000,10]
+
+#print train_Y
+
+test_X = []
+test_another = []
+#print sys.argv
+
+for arg in sys.argv[1:11]:
+	test_another.append(int(arg))
+	#print arg
+	# test_X = np.append(test_X,int(arg))
+
+
+# print test_X
+print test_another
+test_XX = [test_another]
+print test_XX
+
+test_X = np.array(test_XX)
+print test_X
+
+train_normalized_X = preprocessing.normalize(train_X)
+
+train_standardized_X = preprocessing.scale(train_X)
+
+model = RandomForestClassifier(n_estimators=200)
+model.fit(train_X,train_Y)
+
+prediction = model.predict(test_X)
+
+print prediction
+
+#cmdargs = str(sys.argv)
+
+#print ("Args List: %s" % cmdargs)
+
+
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -13,12 +13,29 @@ app.use('/', express.static(__dirname + '/public'));
 
 app.get('/compute', function(request,response){
 
+	//console.log(request);
+	var args = [];
+	args[0] = request.query["elevation"];
+	args[1] = request.query["aspect"];
+	args[2] = request.query["slope"];
+	args[3] = request.query["hor_water"];
+	args[4] = request.query["ver_water"];
+	args[5] = request.query["hor_road"];
+	args[6] = request.query["hill_9"];
+	args[7] = request.query["hill_12"];
+	args[8] = request.query["hill_3"];
+	args[9] = request.query["hor_fire"];
+
 	var options = {
+		args: args
 	}
-	
-	PythonShell.run('./hello.py', function (err,results) {
+	//console.log(options);
+
+	console.log(request.query);
+	PythonShell.run('./random_forest.py', options, function (err,results) {
 	  if (err) throw err;
 	  console.log(results);
+	  response.send(results);
 	});
 })
 
